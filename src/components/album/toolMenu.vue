e4770d010fc8d3816e0e786f8ac1a5d1a9f2b33c
ShineLuLu <fancyyard@163.com>
2020-02-25 22:45:22 +0800

    添加定位测试

--- src/components/album/toolMenu.vue
+++ src/components/album/toolMenu.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="tool-menu-panel">
     <el-button type="text">相册</el-button>
+    <el-button type="text" @click="flyToGFHome">猪儿</el-button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    flyToGFHome() {
+      // let sphere = new Cesium.BoundingSphere(
+      //   Cesium.Cartesian3.fromDegrees(108.2394, 34.5347),
+      //   1e3
+      // );
+      // earth.camera.flyToBoundingSphere(sphere);
+      earth.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(108.23944444444444, 34.53472222222222, 1e3),
+        // orientation: {
+        //   heading: Cesium.Math.toRadians(0),
+        //   pitch: Cesium.Math.toRadians(-35.0),
+        //   roll: 0.0
+        // }
+      });
+    }
+  }
+};
 </script>
 
 <style>
--- src/components/home/earthHome.vue
+++ src/components/home/earthHome.vue
@@ -16,16 +16,22 @@ export default {
       earth = new Cesium.Viewer("earth-main-panel", {
         terrainProvider: Cesium.createWorldTerrain({
           requestWaterMask: true,
-          requestVertexNormals: true    //光照信息
+          requestVertexNormals: true //光照信息
         }),
         imageryProvider: new Cesium.UrlTemplateImageryProvider({
-          url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}",
+          // 地图模式1
+          // url: "https://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}&scale=1&ltype=11",
+          // 地图模式2
+          url: "http://webrd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}"
+          // 卫星模式
+          // url: "https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}",
           // tilingScheme: new Cesium.WebMercatorTilingScheme()
           // minimumLevel: 1,
           // maximumLevel: 20
         }),
         animation: false,
         timeline: false,
+        baseLayerPicker: false,     //隱藏基礎影像選擇
         // fullscreenButton: false,
         navigationHelpButton: false
       });
